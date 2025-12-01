f17c0b3d9469a01fd01ee1a2a9b1543d245cc2ae
vexusware <picaaaaaaaaaaaaa@gmail.com>
2025-12-01 11:30:05 +0700

    Update main.lua

--- src/vexus/main.lua
+++ src/vexus/main.lua
@@ -1,3 +1,6 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vexusware/vexusware.github.io/refs/heads/main/src/vexus/notif.vexusware.lua"))()
+
+
 local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/vexusware/vexusware.github.io/refs/heads/main/src/vexus/vexusware.lua"))()
 
 local URL = Games[game.GameId]
