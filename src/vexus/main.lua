56940aa69057f3d546b7bef349fb2b821573540f
vexusware <picaaaaaaaaaaaaa@gmail.com>
2025-12-01 11:22:45 +0700

    Update main.lua

--- src/vexus/main.lua
+++ src/vexus/main.lua
@@ -1,52 +1,7 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vexusware/vexusware.github.io/refs/heads/main/src/vexus/notif.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
 
-local function SafeRequest(url)
-    local response
+local URL = Games[game.GameId]
 
-    if request then
-        local r = request({Url = url, Method = "GET"})
-        response = r and r.Body
-    elseif http and http.request then
-        local r = http.request({Url = url, Method = "GET"})
-        response = r and r.Body
-    elseif (syn and syn.request) then
-        local r = syn.request({Url = url, Method = "GET"})
-        response = r and r.Body
-    elseif (fluxus and fluxus.request) then
-        local r = fluxus.request({Url = url, Method = "GET"})
-        response = r and r.Body
-    elseif (delta and delta.request) then
-        local r = delta.request({Url = url, Method = "GET"})
-        response = r and r.Body
-
-    elseif game and game.HttpGet then
-        local ok, result = pcall(function()
-            return game:HttpGet(url)
-        end)
-        response = ok and result or nil
-    end
-
-    return response
+if URL then
+  loadstring(game:HttpGet(URL))()
 end
-
-local function SafeGet(url)
-    local ok, result = pcall(function()
-        return SafeRequest(url)
-    end)
-    return ok and result or nil
-end
-
-local dataURL = "https://raw.githubusercontent.com/vexusware/vexusware.github.io/refs/heads/main/src/vexus/vexusware.lua"
-local database = SafeGet(dataURL)
-if not database then return end
-
-local Games = loadstring(database)()
-if not Games then return end
-
-local URL = Games[game.PlaceId]
-if not URL then return end
-
-local scriptData = SafeGet(URL)
-if not scriptData then return end
-
-loadstring(scriptData)()
